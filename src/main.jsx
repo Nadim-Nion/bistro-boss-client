49a3c4f5d0d1a43daec0f4396c1b958439ec5926
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-20 14:16:51 +0600

    dynamic page title using route

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
+
 
 import { router } from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </React.StrictMode>,
 )
