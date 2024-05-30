db6c22d99afcc3ce904f8508d38e13e1d204919e
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-30 22:27:27 +0600

    verify token and axios interceptors

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -7,6 +7,22 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use((config) => {
+        const token = localStorage.getItem('access-token');
+        console.log('request stopped by interceptors', token);
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, (error) => {
+        console.log(error);
+    });
+
+    axiosSecure.interceptors.response.use((response) => {
+        return response;
+    }, (error) => {
+        const status = error.response.status;
+        console.log('Status error in the response interceptors', status);
+        return Promise.reject(error);
+    })
     return axiosSecure;
 };
 
